be8ee52a848076d08174221be48321b63dbf7d67
Bogdan Arsenic <bogdanarsenic@gmail.com>
2020-06-21 19:36:42 +0200

    Adding Admins from txt file

--- TicketReservation/TicketReservation/Global.asax.cs
+++ TicketReservation/TicketReservation/Global.asax.cs
@@ -5,6 +5,8 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using TicketReservation.Models;
+using TicketReservation.ModelsDB;
 
 namespace TicketReservation
 {
@@ -16,6 +18,31 @@ namespace TicketReservation
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            LoadAdmins();
+        }
+
+        public void LoadAdmins()
+        {
+            UserDB userDB = new UserDB();
+            User admin = new User();
+
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\Bogdan\Desktop\Web-2020\Admin.txt");
+
+
+            foreach (string line in lines)
+            {
+                string[] nesto = line.Split(',');
+
+                admin.Username = nesto[0];
+                admin.Password = nesto[1];
+                admin.Name = nesto[2];
+                admin.Surname = nesto[3];
+                admin.Gender = nesto[4];
+                admin.Role = "Admin";
+
+                userDB.Insert(admin);
+            }
         }
     }
 }
